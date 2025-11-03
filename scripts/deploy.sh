b81206801061fa3434e88bd3f5735692578aa6ab
Venkata Marella <theincrediblecloud@gmail.com>
2025-11-03 10:30:04 -0600

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,37 +8,14 @@ RAG_API_URL=${RAG_API_URL:-"https://your-fastapi.example.com/chat"}
 : "${SLACK_BOT_TOKEN_ARN:?Need SLACK_BOT_TOKEN_ARN env var}"
 : "${SLACK_SIGNING_SECRET_ARN:?Need SLACK_SIGNING_SECRET_ARN env var}"
 
-# sam build
-# sam deploy \
-#   --stack-name slack-rag \
-#   --region "$AWS_REGION" \
-#   --capabilities CAPABILITY_IAM \
-#   --parameter-overrides \
-#     RagApiUrl="$RAG_API_URL" \
-#     ArtifactsBucket="$ARTIFACTS_BUCKET" \
-#     SlackBotTokenArn="$SLACK_BOT_TOKEN_ARN" \
-#     SlackSigningSecretArn="$SLACK_SIGNING_SECRET_ARN"
-
 sam build -t infra/sam/template.yaml
-sam deploy \
-  -t infra/sam/template.yaml \
-  --stack-name slack-rag \
-  --region us-east-1 \
-  --capabilities CAPABILITY_IAM \
-  --resolve-s3 \
-  --parameter-overrides \
-    ArtifactsBucket="slack-rag-artifacts-73918652" \
-    RagApiUrl="https://6edg14q4fh.execute-api.us-east-1.amazonaws.com/Prod/chat" \
-    SlackBotTokenArn="arn:aws:secretsmanager:us-east-1:471112701253:secret:rag/slack/bot_token-tZy5lT" \
-    SlackSigningSecretArn="arn:aws:secretsmanager:us-east-1:471112701253:secret:slack/signing-secret-n7WDeM"
-
-# sam deploy -t infra/sam/template.yaml \
-#   --stack-name slack-rag \
-#   --region us-east-1 \
-#   --capabilities CAPABILITY_IAM \
-#   --resolve-s3 \
-#   --parameter-overrides \
-#     RagApiUrl=$RAG_API_URL \
-#     ArtifactsBucket=$ARTIFACTS_BUCKET \
-#     SlackBotTokenArn=$SLACK_BOT_TOKEN_ARN \
-#     SlackSigningSecretArn=$SLACK_SIGNING_SECRET_ARN
+sam deploy -t infra/sam/template.yaml \
+   --stack-name slack-rag \
+   --region us-east-1 \
+   --capabilities CAPABILITY_IAM \
+   --resolve-s3 \
+   --parameter-overrides \
+     RagApiUrl=$RAG_API_URL \
+     ArtifactsBucket=$ARTIFACTS_BUCKET \
+     SlackBotTokenArn=$SLACK_BOT_TOKEN_ARN \
+     SlackSigningSecretArn=$SLACK_SIGNING_SECRET_ARN
